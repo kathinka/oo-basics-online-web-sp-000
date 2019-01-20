addeb1b2631edd50288397fee0c1c9d26493fa42
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-20 16:24:35 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,5 +19,10 @@ attr_accessor(:brand, :color, :size, :material, :condition)
   def initialize(brand)
     @brand = brand
   end
-  
+
+  def cobble
+puts "the shoe has been repaired"
+@condition = new
+  end
+
 end
