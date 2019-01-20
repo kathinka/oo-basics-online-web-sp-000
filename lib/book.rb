b27c399309e872d0d36d0f19866cfafb2b68bead
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-20 16:27:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -24,6 +24,9 @@ attr_accessor(:brand, :color, :size, :material, :condition)
     @condition = "new"
   end
 
+  def condition@condition
+  end
+
   def cobble
 puts "Your shoe is as good as new!"
 @condition = "new"
