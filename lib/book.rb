a45fba209c313ab13db80a2f3e4741932826464a
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-20 16:23:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,11 +4,20 @@ class Book
 attr_accessor(:title, :author, :page_count, :genre)
 
   def initialize(title)
-  @title = title
+    @title = title
   end
 
   def turn_page
-  puts "Flipping the page...wow, you read fast!"
+    puts "Flipping the page...wow, you read fast!"
   end
 
+end
+
+class Shoe
+attr_accessor(:brand, :color, :size, :material, :condition)
+
+  def initialize(brand)
+    @brand = brand
+  end
+  
 end
